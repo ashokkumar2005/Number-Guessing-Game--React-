f2bc19e24a3ed178b3c1035836b16196ffc48fa4
Ashokkumar <babajiashok8637@gmail.com>
2026-04-08 14:16:38 +0530

    Updated project files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Result from "./result"
+import "./index.css"
 
 function App() {
    
@@ -7,43 +8,55 @@ function App() {
     const [text,setText] =useState("");
     const [randomnum,setRandomnum] = useState(0)
     const [attempt,setAttempt] = useState(0)
+    const [guess,setGuess] = useState(null);
     useEffect(()=>{
        genraterandomnum();
 
     },[])
 
 function genraterandomnum(){
-        const randomnum = Math.floor( Math.random()*10+1);
-        setRandomnum(randomnum)
+        const num = Math.floor( Math.random()*10+1);
+        setRandomnum(num)
 }
     function handlechange(e){
      setText(e.target.value);
     }
 
-function handlesubmit(){
+function handlesubmit(e){
+    e.preventDefault();
 
     if(text === "") return;
 
     const userguess = Number(text);
+    setGuess(userguess);
+    setAttempt(prev=>prev+1);
+   
+
+    if(userguess === randomnum){
+     setTimeout(()=>{setAttempt(0);
+        genraterandomnum();
+        setText("")
+        setGuess(null)}, 1000)
+        
+    }else{
+        setText("");
+    }
+
 
     
 }
-
-    
-    
-    
-     
-    
-    return(
+  
+     return(
       <>
       <form onSubmit={handlesubmit}>
-      <label> Guess the number between 1-10 : answer : {randomnum} </label><br/>
-      <input 
-      type="number"
-      placeholder="Enter a Number" 
-      onChange={handlechange}/>
-      
-      <button type="submit"> Check Guess</button>
+      <label> Guess the number between 1-10 </label><br/>
+<input
+  type="number"
+  value={text}
+  onChange={handlechange}
+  placeholder="Enter a Number"
+/>      
+      <button type="submit" disabled= {!text} > Check Guess</button>
       </form>
       <Result randomnum={randomnum} text={guess} attempt={attempt}></Result>
       </>
--- src/result.jsx
+++ src/result.jsx
@@ -1,29 +1,28 @@
-
+import "./index.css"
 function Result(props){
 
     const {text,randomnum,attempt}=props
 
     let message = "";
 
-    if(text === ""){
+    if(text === null){
         message = "Enter a number"
     }
 
-    if(text>randomnum){
+    else if(text>randomnum){
         message = "Input Number Is High"
     }
     else if(text < randomnum){
         message = "Input Number Is LOW"
     }
-    else if ( text === randomnum){
+    else if( text === randomnum) {
         message = "Ahhh That's Correct"
-        
-    }
+        }
    return(
-    <>
+    <div className="container">
     <h2>{message}</h2>
     <h2>attempt : {attempt}</h2>
-    </>
+    </div>
    )
 }
 export default Result;
